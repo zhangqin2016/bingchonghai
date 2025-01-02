74ab6b25d55ff86e0d819451648a650e6f0ff4b5
zhangqin <492297036@qq.com>
2025-01-02 18:13:15 +0800

    test

--- test.py
+++ test.py
@@ -52,7 +52,7 @@ def generate_insert_statements(cities, country_code):
         name = city['name']
         province_code = country_code  # 根据需要填写
         insert_cites_sql.append(
-            f"INSERT INTO `fff_cites` (`code`, `name`, `province_code`) VALUES ('NP_{code}', '{name}', '{province_code}');")
+            f"INSERT INTO `split_table_info` (`split_key`, `split_table`) VALUES ('NP_{code}', 'fff_address_weather_log_split_guowai');")
 
         # fff_address_latlng 表的插入语句
         lat = city['lat']
